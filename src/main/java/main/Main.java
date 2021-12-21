333c2d802b8a8ac6a03db27023d58936211ac959
Максим Карачун <maksim.karachun@bk.ru>
2021-12-21 23:01:57 +0300

    Убран лишний код в main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,5 @@
 package main;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
-import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -11,13 +7,6 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @PropertySource("classpath:application.yml")
 public class Main {
-
-  @PostConstruct
-  void started() {
-    Calendar calendar = new GregorianCalendar();
-    TimeZone timeZone = calendar.getTimeZone();
-    TimeZone.setDefault(timeZone);
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
